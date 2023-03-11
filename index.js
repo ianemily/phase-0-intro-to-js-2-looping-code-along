efcdd8a03e90f932711044e4b8e10f804481a5b6
Emily Iannizzi <eiannizzi18@gmail.com>
2023-03-10 21:10:01 -0500

    completed looping lab

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const messageNames = [];
+
+function writeCards(names, value) {
+    var cards = [];
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+    }
+    return cards;
+}
+
+function countDown(value) {
+    for (let i = value; i >= 0; i--) {   
+        console.log(i); 
+    }
+}
